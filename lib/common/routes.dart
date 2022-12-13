3083429f07d3c2c174c69deb42c8a47f59ff8752
Oleksii <oleksii.melnychenko@extrawest.com>
2022-12-13 21:45:11 +0200

    sign up screen ui implementation

--- lib/common/routes.dart
+++ lib/common/routes.dart
@@ -2,11 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:water_tracker_app/common/ui/screens/splash_screen.dart';
 
 import '../features/feature_login/screens/signin_screen.dart';
+import '../features/feature_login/screens/singup_screen.dart';
 
 const splashScreenRoute = 'splash_screen';
 const signInScreenRoute = 'signIn_screen';
+const signUpScreenRoute = 'signUp_screen';
 
 final applicationRoutes = <String, WidgetBuilder>{
-  'splash_screen': (BuildContext context) => const SplashScreen(),
-  'signIn_screen': (BuildContext context) => const SignInScreen(),
+  splashScreenRoute: (BuildContext context) => const SplashScreen(),
+  signInScreenRoute: (BuildContext context) => const SignInScreen(),
+  signUpScreenRoute: (BuildContext context) => const SignUpScreen(),
 };
--- lib/features/feature_login/screens/singup_screen.dart
+++ lib/features/feature_login/screens/singup_screen.dart
@@ -1,15 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:water_tracker_app/common/constants.dart';
 import 'package:water_tracker_app/common/routes.dart';
-import 'package:water_tracker_app/features/feature_login/cubit/login_cubit.dart';
-import 'package:water_tracker_app/features/feature_login/widgets/social_button.dart';
 
+import '../../../common/constants.dart';
+import '../cubit/login_cubit.dart';
 import '../widgets/auth_button.dart';
 import '../widgets/form_input.dart';
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({Key? key}) : super(key: key);
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -45,11 +45,17 @@ class SignInScreen extends StatelessWidget {
                       children: [
                         const SizedBox(height: 32),
                         Text(
-                          'Sign In',
+                          'Sign Up',
                           style: theme.textTheme.headline2,
                           textAlign: TextAlign.center,
                         ),
                         const SizedBox(height: 24),
+                        const FormInput(
+                          label: 'Name',
+                          hint: 'Enter your Name',
+                          icon: Icons.email,
+                        ),
+                        const SizedBox(height: 16),
                         const FormInput(
                           label: 'Email',
                           hint: 'Enter your email',
@@ -62,44 +68,26 @@ class SignInScreen extends StatelessWidget {
                           icon: Icons.lock,
                           isPassword: true,
                         ),
+                        const SizedBox(height: 16),
+                        const FormInput(
+                          label: 'Confirm Password',
+                          hint: 'Confirm your password',
+                          icon: Icons.lock,
+                          isPassword: true,
+                        ),
                         const SizedBox(height: 32),
                         AuthButton(
-                          text: 'LOGIN',
+                          text: 'REGISTER',
                           onPressed: () {
                             //TODO: implement login
                           },
-                        ),
-                        const SizedBox(height: 16),
-                        Text(
-                          'Or sign in with:',
-                          style: theme.textTheme.subtitle1,
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 16),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SocialButton(
-                              asset: Assets.googleLogo,
-                              onPressed: () {
-                                //TODO: Implement Google Sign In
-                              },
-                            ),
-                            const SizedBox(width: 16),
-                            SocialButton(
-                              asset: Assets.facebookLogo,
-                              onPressed: () {
-                                //TODO: Implement Facebook Sign In
-                              },
-                            ),
-                          ],
                         ),
                         const SizedBox(height: 32),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              'Don\'t have an account?',
+                              'Have an account?',
                               style: theme.textTheme.subtitle1,
                             ),
                             TextButton(
@@ -108,10 +96,10 @@ class SignInScreen extends StatelessWidget {
                               ),
                               onPressed: () {
                                 Navigator.of(context)
-                                    .pushReplacementNamed(signUpScreenRoute);
+                                    .pushReplacementNamed(signInScreenRoute);
                               },
                               child: Text(
-                                'Sign Up',
+                                'Sign In',
                                 style: theme.textTheme.subtitle1?.copyWith(
                                   fontWeight: FontWeight.w800,
                                   decoration: TextDecoration.underline,
