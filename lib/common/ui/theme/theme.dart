5e05dcbf45a5dc2a8caeea96213527ae50503bca
Oleksii <oleksii.melnychenko@extrawest.com>
2022-12-19 13:28:11 +0200

    home bottom bar ui implementation

--- lib/common/ui/theme/theme.dart
+++ lib/common/ui/theme/theme.dart
@@ -5,7 +5,8 @@ final applicationTheme = ThemeData(
   colorScheme: const ColorScheme.light(
     primary: Color(0xFF5EB0FF),
     secondary: Color(0xFF0484FF),
-    secondaryContainer: Color(0xFF6daeed)
+    secondaryContainer: Color(0xFF6daeed),
+    background: Color(0xFFf5f5f5),
   ),
   textTheme: const TextTheme(
     headline2: TextStyle(
@@ -28,7 +29,12 @@ final applicationTheme = ThemeData(
       fontWeight: FontWeight.w700,
       color: Color(0xFF0484FF),
     ),
-  )
+    bodyText1: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w800,
+      color: Colors.black87,
+    ),
+  ),
 );
 
 // primary: Color(linear-gradient(360deg, #0484FF -62.7%, #5EB0FF 100%))
---
+++ lib/features/feature_home/widgets/bottom_app_bar.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class BottomApplicationBar extends StatelessWidget {
+  const BottomApplicationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.stacked_bar_chart,
+                      size: 32,
+                      color: theme.colorScheme.secondary,
+                    ),
+                    Text(
+                      'Statistics',
+                      style: theme.textTheme.bodySmall?.copyWith(
+                        fontSize: 15,
+                        color: Colors.black87,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.account_circle_rounded,
+                      size: 32,
+                      color: theme.colorScheme.secondary,
+                    ),
+                    Text(
+                      'Account',
+                      style: theme.textTheme.bodySmall?.copyWith(
+                        fontSize: 15,
+                        color: Colors.black87,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(24),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                theme.colorScheme.primary,
+                theme.colorScheme.secondary,
+              ],
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: theme.colorScheme.secondary.withOpacity(0.4),
+                spreadRadius: 1,
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: IconButton(
+            iconSize: 64,
+            onPressed: () {},
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                const EdgeInsets.all(14),
+              ),
+              shape: MaterialStateProperty.all(
+                const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(24),
+                  ),
+                ),
+              ),
+            ),
+            icon: const Icon(
+              Icons.add_rounded,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
