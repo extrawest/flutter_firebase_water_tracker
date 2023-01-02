c2deb54fbbc5aeb78fa1f453faa558a0f359e403
Oleksii <oleksii.melnychenko@extrawest.com>
2023-01-02 11:47:51 +0200

    firebase crashlytics setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tracker_app/firebase_options.dart';
 
@@ -10,6 +12,24 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseAuth.instance.authStateChanges().listen((firebaseUser) {
+    if (firebaseUser != null && firebaseUser.email != null) {
+      FirebaseCrashlytics.instance.setCustomKey(
+        'email',
+        firebaseUser.email ?? '',
+      );
+    }
+    if (firebaseUser != null) {
+      FirebaseCrashlytics.instance.setUserIdentifier(firebaseUser.uid);
+    }
+    if (firebaseUser != null && firebaseUser.displayName != null) {
+      FirebaseCrashlytics.instance.setCustomKey(
+        'name',
+        firebaseUser.displayName ?? '',
+      );
+    }
+  });
 
   runApp(const WaterTrackerApp());
 }
