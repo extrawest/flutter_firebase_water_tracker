7129658eecf4a5c9bc7be6f87da36d8d0a3bc0fd
Oleksii <oleksii.melnychenko@extrawest.com>
2023-01-09 20:48:30 +0200

    login screen refactoring and codespliting

--- lib/features/feature_login/screens/signin_screen/signin_screen.dart
+++ lib/features/feature_login/screens/signin_screen/signin_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:water_tracker_app/common/constants.dart';
 import 'package:water_tracker_app/common/routes.dart';
+import 'package:water_tracker_app/features/feature_login/widgets/login_scaffold.dart';
 import 'package:water_tracker_app/features/feature_login/widgets/social_button.dart';
 
 import '../../repositories/login_repository.dart';
@@ -22,143 +23,111 @@ class SignInScreen extends StatelessWidget {
       ),
       child: Builder(
         builder: (context) {
-          return Scaffold(
-            body: BlocListener<SignInCubit, SignInState>(
-              listener: (context, state) {
-                if (state.isSignedIn) {
-                  Navigator.of(context).pushReplacementNamed(homeScreenRoute);
-                }
-                else if (state.error.isNotEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(state.error),
-                      duration: const Duration(seconds: 5),
-                    ),
-                  );
-                }
-              },
-              child: GestureDetector(
-                onTap: () => FocusScope.of(context).unfocus(),
-                child: Stack(
+          return LoginScaffold<SignInCubit, SignInState>(
+            listener: (context, state) {
+              if (state.isSignedIn) {
+                Navigator.of(context).pushReplacementNamed(homeScreenRoute);
+              } else if (state.error.isNotEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(state.error),
+                    duration: const Duration(seconds: 5),
+                  ),
+                );
+              }
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 32),
+                Text(
+                  'Sign In',
+                  style: theme.textTheme.headline2,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 24),
+                FormInput(
+                  label: 'Email',
+                  hint: 'Enter your email',
+                  icon: Icons.email,
+                  onChanged:
+                  context.read<SignInCubit>().onEmailChanged,
+                ),
+                const SizedBox(height: 16),
+                FormInput(
+                  label: 'Password',
+                  hint: 'Enter your password',
+                  icon: Icons.lock,
+                  isPassword: true,
+                  onHidePassword: context
+                      .read<SignInCubit>()
+                      .togglePasswordObscure,
+                  isPasswordObscured: context
+                      .watch<SignInCubit>()
+                      .state
+                      .isPasswordObscured,
+                  onChanged:
+                  context.read<SignInCubit>().onPasswordChanged,
+                ),
+                const SizedBox(height: 32),
+                AuthButton(
+                  text: 'LOGIN',
+                  onPressed:() async {
+                    await context.read<SignInCubit>().onLoginPressed();
+                  },
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Or sign in with:',
+                  style: theme.textTheme.subtitle1,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            theme.colorScheme.primary,
-                            theme.colorScheme.secondary,
-                          ],
-                        ),
-                      ),
+                    SocialButton(
+                      asset: Assets.googleLogo,
+                      onPressed: () async {
+                        await context.read<SignInCubit>().onGoogleLoginPressed();
+                      },
                     ),
-                    SingleChildScrollView(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: Dimensions.horizontalPadding),
-                        child: SafeArea(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const SizedBox(height: 32),
-                              Text(
-                                'Sign In',
-                                style: theme.textTheme.headline2,
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 24),
-                              FormInput(
-                                label: 'Email',
-                                hint: 'Enter your email',
-                                icon: Icons.email,
-                                onChanged:
-                                    context.read<SignInCubit>().onEmailChanged,
-                              ),
-                              const SizedBox(height: 16),
-                              FormInput(
-                                label: 'Password',
-                                hint: 'Enter your password',
-                                icon: Icons.lock,
-                                isPassword: true,
-                                onHidePassword: context
-                                    .read<SignInCubit>()
-                                    .togglePasswordObscure,
-                                isPasswordObscured: context
-                                    .watch<SignInCubit>()
-                                    .state
-                                    .isPasswordObscured,
-                                onChanged:
-                                    context.read<SignInCubit>().onPasswordChanged,
-                              ),
-                              const SizedBox(height: 32),
-                              AuthButton(
-                                text: 'LOGIN',
-                                onPressed:() async {
-                                  await context.read<SignInCubit>().onLoginPressed();
-                                },
-                              ),
-                              const SizedBox(height: 16),
-                              Text(
-                                'Or sign in with:',
-                                style: theme.textTheme.subtitle1,
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 16),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SocialButton(
-                                    asset: Assets.googleLogo,
-                                    onPressed: () async {
-                                      await context.read<SignInCubit>().onGoogleLoginPressed();
-                                    },
-                                  ),
-                                  const SizedBox(width: 16),
-                                  SocialButton(
-                                    asset: Assets.facebookLogo,
-                                    onPressed: () async {
-                                      await context.read<SignInCubit>().onFacebookLoginPressed();
-                                    },
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(height: 32),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Don\'t have an account?',
-                                    style: theme.textTheme.subtitle1,
-                                  ),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      padding: const EdgeInsets.all(0),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .pushReplacementNamed(signUpScreenRoute);
-                                    },
-                                    child: Text(
-                                      'Sign Up',
-                                      style: theme.textTheme.subtitle1?.copyWith(
-                                        fontWeight: FontWeight.w800,
-                                        decoration: TextDecoration.underline,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    const SizedBox(width: 16),
+                    SocialButton(
+                      asset: Assets.facebookLogo,
+                      onPressed: () async {
+                        await context.read<SignInCubit>().onFacebookLoginPressed();
+                      },
                     ),
                   ],
                 ),
-              ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: theme.textTheme.subtitle1,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(0),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushReplacementNamed(signUpScreenRoute);
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: theme.textTheme.subtitle1?.copyWith(
+                          fontWeight: FontWeight.w800,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
           );
         }
--- lib/features/feature_login/screens/signup_screen/singup_screen.dart
+++ lib/features/feature_login/screens/signup_screen/singup_screen.dart
@@ -4,8 +4,8 @@ import 'package:water_tracker_app/common/routes.dart';
 import 'package:water_tracker_app/features/feature_login/repositories/login_repository.dart';
 import 'package:water_tracker_app/features/feature_login/screens/signup_screen/signup_cubit.dart';
 import 'package:water_tracker_app/features/feature_login/screens/signup_screen/signup_state.dart';
+import 'package:water_tracker_app/features/feature_login/widgets/login_scaffold.dart';
 
-import '../../../../common/constants.dart';
 import '../../widgets/auth_button.dart';
 import '../../widgets/form_input.dart';
 
@@ -22,9 +22,8 @@ class SignUpScreen extends StatelessWidget {
       child: Builder(
         builder: (context) {
           final cubit = context.read<SignUpCubit>();
-          return Scaffold(
-            body: BlocListener<SignUpCubit, SignUpState>(
-              listener: (context, state) {
+          return LoginScaffold<SignUpCubit, SignUpState>(
+              listener: (context, state){
                 if (state.isSignedUp) {
                   Navigator.of(context).pushReplacementNamed(homeScreenRoute);
                 } else if (state.error.isNotEmpty) {
@@ -36,121 +35,91 @@ class SignUpScreen extends StatelessWidget {
                   );
                 }
               },
-              child: GestureDetector(
-                onTap: () => FocusScope.of(context).unfocus(),
-                child: Stack(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            theme.colorScheme.primary,
-                            theme.colorScheme.secondary,
-                          ],
-                        ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 32),
+                  Text(
+                    'Sign Up',
+                    style: theme.textTheme.headline2,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 24),
+                  FormInput(
+                    label: 'Name',
+                    hint: 'Enter your Name',
+                    icon: Icons.email,
+                    onChanged: cubit.onNameChanged,
+                  ),
+                  const SizedBox(height: 16),
+                  FormInput(
+                      label: 'Email',
+                      hint: 'Enter your email',
+                      icon: Icons.email,
+                      onChanged: cubit.onEmailChanged),
+                  const SizedBox(height: 16),
+                  FormInput(
+                      label: 'Password',
+                      hint: 'Enter your password',
+                      icon: Icons.lock,
+                      isPassword: true,
+                      onHidePassword: context
+                          .read<SignUpCubit>()
+                          .togglePasswordObscure,
+                      isPasswordObscured: context
+                          .watch<SignUpCubit>()
+                          .state
+                          .isPasswordObscured,
+                      onChanged: cubit.onPasswordChanged),
+                  const SizedBox(height: 16),
+                  FormInput(
+                      label: 'Confirm Password',
+                      hint: 'Confirm your password',
+                      icon: Icons.lock,
+                      isPassword: true,
+                      onHidePassword: cubit.togglePasswordObscure,
+                      isPasswordObscured: context
+                          .watch<SignUpCubit>()
+                          .state
+                          .isPasswordObscured,
+                      onChanged: cubit.onConfirmPasswordChanged),
+                  const SizedBox(height: 32),
+                  AuthButton(
+                    text: 'REGISTER',
+                    onPressed: () async {
+                      await cubit.onRegisterButtonPressed();
+                    },
+                  ),
+                  const SizedBox(height: 32),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Have an account?',
+                        style: theme.textTheme.subtitle1,
                       ),
-                    ),
-                    SingleChildScrollView(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: Dimensions.horizontalPadding),
-                        child: SafeArea(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const SizedBox(height: 32),
-                              Text(
-                                'Sign Up',
-                                style: theme.textTheme.headline2,
-                                textAlign: TextAlign.center,
-                              ),
-                              const SizedBox(height: 24),
-                              FormInput(
-                                label: 'Name',
-                                hint: 'Enter your Name',
-                                icon: Icons.email,
-                                onChanged: cubit.onNameChanged,
-                              ),
-                              const SizedBox(height: 16),
-                              FormInput(
-                                  label: 'Email',
-                                  hint: 'Enter your email',
-                                  icon: Icons.email,
-                                  onChanged: cubit.onEmailChanged),
-                              const SizedBox(height: 16),
-                              FormInput(
-                                  label: 'Password',
-                                  hint: 'Enter your password',
-                                  icon: Icons.lock,
-                                  isPassword: true,
-                                  onHidePassword: context
-                                      .read<SignUpCubit>()
-                                      .togglePasswordObscure,
-                                  isPasswordObscured: context
-                                      .watch<SignUpCubit>()
-                                      .state
-                                      .isPasswordObscured,
-                                  onChanged: cubit.onPasswordChanged),
-                              const SizedBox(height: 16),
-                              FormInput(
-                                  label: 'Confirm Password',
-                                  hint: 'Confirm your password',
-                                  icon: Icons.lock,
-                                  isPassword: true,
-                                  onHidePassword: cubit.togglePasswordObscure,
-                                  isPasswordObscured: context
-                                      .watch<SignUpCubit>()
-                                      .state
-                                      .isPasswordObscured,
-                                  onChanged: cubit.onConfirmPasswordChanged),
-                              const SizedBox(height: 32),
-                              AuthButton(
-                                text: 'REGISTER',
-                                onPressed: () async {
-                                  await cubit.onRegisterButtonPressed();
-                                },
-                              ),
-                              const SizedBox(height: 32),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    'Have an account?',
-                                    style: theme.textTheme.subtitle1,
-                                  ),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      padding: const EdgeInsets.all(0),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .pushReplacementNamed(
-                                              signInScreenRoute);
-                                    },
-                                    child: Text(
-                                      'Sign In',
-                                      style:
-                                          theme.textTheme.subtitle1?.copyWith(
-                                        fontWeight: FontWeight.w800,
-                                        decoration: TextDecoration.underline,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(0),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context)
+                              .pushReplacementNamed(
+                              signInScreenRoute);
+                        },
+                        child: Text(
+                          'Sign In',
+                          style:
+                          theme.textTheme.subtitle1?.copyWith(
+                            fontWeight: FontWeight.w800,
+                            decoration: TextDecoration.underline,
                           ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
+                      )
+                    ],
+                  ),
+                ],
               ),
-            ),
           );
         },
       ),
