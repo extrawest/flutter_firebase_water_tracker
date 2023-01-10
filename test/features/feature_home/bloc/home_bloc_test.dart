adf14d85d01ad039ea57804cc665d44d79223767
Oleksii <oleksii.melnychenko@extrawest.com>
2023-01-10 04:42:28 +0200

    added unit tests for core repositories and blocs

---
+++ test/features/feature_home/bloc/home_bloc_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:water_tracker_app/features/feature_home/bloc/home_bloc.dart';
+import 'package:water_tracker_app/features/feature_home/models/drink_model.dart';
+import 'package:water_tracker_app/features/feature_home/repositories/home_repository.dart';
+
+import 'home_bloc_test.mocks.dart';
+
+@GenerateMocks([HomeRepositoryImpl])
+void main() {
+  late HomeBloc homeBloc;
+  late MockHomeRepositoryImpl mockHomeRepositoryImpl;
+
+  setUp(() {
+    mockHomeRepositoryImpl = MockHomeRepositoryImpl();
+    homeBloc = HomeBloc(mockHomeRepositoryImpl);
+  });
+
+  test('should calculate daily progress base on current intake', () {
+    const currentIntake = 1000;
+    const goal = 2000;
+    const expectedProgress = currentIntake > goal ? goal : currentIntake / goal;
+
+    expect(homeBloc.calculateProgress(currentIntake, goal), expectedProgress);
+  });
+
+  test('should calculate overall volume of consumed drinks over a day', () {
+    final drinks = [
+      DrinkModel(name: 'name', amount: 50, timestamp: 'timestamp'),
+      DrinkModel(name: 'name', amount: 50, timestamp: 'timestamp'),
+      DrinkModel(name: 'name', amount: 50, timestamp: 'timestamp'),
+      DrinkModel(name: 'name', amount: 50, timestamp: 'timestamp'),
+    ];
+    final expected = drinks.fold<int>(0, (previousValue, element) => previousValue + element.amount);
+    expect(homeBloc.calculateOverallVolume(drinks), expected);
+  });
+}
---
+++ test/features/feature_home/repositories/home_repository_test.dart
@@ -0,0 +1,83 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:water_tracker_app/features/feature_home/models/drink_model.dart';
+import 'package:water_tracker_app/features/feature_home/models/user_model.dart';
+import 'package:water_tracker_app/features/feature_home/repositories/home_repository.dart';
+
+import 'home_repository_test.mocks.dart';
+
+@GenerateMocks([HomeRepository])
+void main() {
+  late MockHomeRepository mockHomeRepository;
+
+  setUp(() {
+    mockHomeRepository = MockHomeRepository();
+  });
+
+  test('should retrieve progress indicator type from remote config', () async {
+    when(mockHomeRepository.getProgressIndicatorType())
+        .thenAnswer((_) async => 'circular');
+
+    final progressIndicatorType =
+        await mockHomeRepository.getProgressIndicatorType();
+
+    expect(progressIndicatorType, 'circular');
+  });
+
+  group('Firestore', () {
+    test('should retrieve user model from firestore', () async {
+      when(mockHomeRepository.userStream()).thenAnswer(
+        (_) => Stream.periodic(
+          const Duration(seconds: 1),
+          (_) {
+            return const UserModel(
+                name: 'name',
+                email: 'email',
+                photoUrl: 'photoUrl',
+                dailyGoal: 4000);
+          },
+        ),
+      );
+
+      final userStream = mockHomeRepository.userStream();
+
+      expect(userStream, isA<Stream<UserModel>>());
+    });
+
+    test('should retrieve drink model from firestore', () async {
+      when(mockHomeRepository.drinksStream()).thenAnswer(
+        (_) => Stream.periodic(
+          const Duration(seconds: 1),
+          (_) {
+            return [
+              DrinkModel(
+                name: 'name',
+                amount: 100,
+                timestamp: 'timestamp',
+              )
+            ];
+          },
+        ),
+      );
+
+      final drinksStream = mockHomeRepository.drinksStream();
+
+      expect(drinksStream, isA<Stream<List<DrinkModel>>>());
+    });
+
+    test('should add drink to firestore', () async {
+      when(mockHomeRepository.addDrink(
+        drinkAmount: 100,
+        drinkName: 'name',
+      )).thenAnswer((_) async => true);
+
+      final isAdded = await mockHomeRepository.addDrink(
+        drinkAmount: 100,
+        drinkName: 'name',
+      );
+
+      expect(isAdded, true);
+    });
+  });
+}
